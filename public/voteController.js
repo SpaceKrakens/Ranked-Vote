476c710ec3f0fae1ff2bbd3df4e5affef4e2d712
Paul Siedler <p0wlomat@gmail.com>
2016-09-22 22:03:56 +0200

    Fix VoteController to do what was intended (I think), add some styling to the templates, make layout.ejs use the partials

--- public/voteController.js
+++ public/voteController.js
@@ -1,35 +1,30 @@
-  // sets lists as sortable and disableSelection
-  $( function() {
-    $( "#sortable1, #sortable2" ).sortable({
-      connectWith: ".connectedSortable"
-    }).disableSelection();
-    // Runs a function when the submitVote button is pressed
-    $( "#savebutton" ).click(postList());
-  } );
-
 // Using the core $.ajax() method
-function postList(){
-  $.ajax({
+var postList = function () {
 
-      url: "server.js",
+};
 
-      data: $( "#sortable2" ).sortable( "serialize", { key: "sort" } ),
+// sets lists as sortable and disableSelection
+$("#sortable1, #sortable2").sortable({
+    connectWith: ".connectedSortable"
+}).disableSelection();
 
-      type: "POST",
-  
-      dataType : "string",
-  })
-    .done(function( json ) {
-      $( "<h1>" ).text( json.title ).appendTo( "body" );
-      $( "<div class=\"content\">").html( json.html ).appendTo( "body" );
-    })
-    .fail(function( xhr, status, errorThrown ) {
-      alert( "Sorry, there was a problem!" );
-      console.log( "Error: " + errorThrown );
-      console.log( "Status: " + status );
-      console.dir( xhr );
-    })
-    .always(function( xhr, status ) {
-      alert( "The request is complete!" );
-    });}
-    
+$("#submitButton").click(function () {
+    $.post({
+            url: "vote/submit",
+            data: $("#sortable2").sortable("serialize", {key: "sort"}),
+            dataType: "json",
+        })
+        .done(function (json) {
+            $("<h1>").text(json.title).appendTo("body");
+            $("<div class=\"content\">").html(json.html).appendTo("body");
+        })
+        .fail(function (xhr, status, errorThrown) {
+            alert("Sorry, there was a problem!");
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        })
+        .always(function (xhr, status) {
+            alert("The request is complete!");
+        });
+});
